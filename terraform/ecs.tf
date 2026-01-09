8370c241f4c7f208d085b0c506cf71bc341b4471
sumedhirey003 <sumedhhirey003@gmail.com>
2026-01-09 22:49:37 +0530

    feat: add ECS cluster and task definition

---
+++ terraform/ecs.tf
@@ -0,0 +1,47 @@
+# ecs cluster
+resource "aws_ecs_cluster" "this" {
+  name = "${var.project_name}-cluster"
+}
+
+# cloudwatch log group for ecs
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/${var.project_name}"
+  retention_in_days = 7
+}
+
+# ecs task definition
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.project_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "flask-app"
+      image = "${aws_ecr_repository.app.repository_url}:latest"
+
+      portMappings = [
+        {
+          containerPort = 5000
+          protocol      = "tcp"
+        }
+      ]
+
+      essential = true
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
